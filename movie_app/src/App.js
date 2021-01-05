4e565af2718be3df4cdc886dcc635bbed0246630
leo <algo2000@naver.com>
2021-01-05 23:45:09 +0900

    update  Reusable Components with JSX + Props

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import Potato from './Potato'
+
+function Food({ favorite }) {
+  return <h1>I Like {favorite}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!!!</h1>
-      <Potato/>
+      <Food favorite ='Kimchi' />
+      <Food favorite='Kimchi1' />
+      <Food favorite='Kimchi2' />
+      <Food favorite='Kimchi3' />
+      <Food favorite='Kimchi4' />
     </div>
   );
 }
