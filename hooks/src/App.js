f8dcef2a263fbdcb59f4a2bb5b65fc425ccc55fa
leo <algo2000@naver.com>
2021-01-06 22:06:54 +0900

    update UseInput Part One

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,33 @@
 import { useState } from "react";
+
+const useInput = (initialVale) => {
+  const [value, setValue] = useState(initialVale);
+  const onChange = event => {
+    console.log(event.target)
+  }
+  return { value, onChange }
+}
+
 const App = () => {
+  // UseState
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1)
   const decrementItem = () => setItem(item - 1)
+
+  // UseInpue
+  const name = useInput('Mr.')
+
   return (
     <div className='App'>
-      <h1>Hello {item}</h1>
-      <h2>start coding</h2>
-      <button onClick={decrementItem}>Decrement</button>
-      <button onClick={incrementItem}>Increment</button>
+      <div>
+        <h1>1. UseState</h1>
+        <button onClick={decrementItem}>Decrement</button>
+        <button onClick={incrementItem}>Increment</button>
+      </div>
+      <div>
+        <h1>2. UseInput</h1>
+        <input placeholder='Name' {...name} />
+      </div>
     </div>
   );
 }
